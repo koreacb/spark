8e4a32202a4357a6cae3b0f014480f45e3d7e514
KW, Chung <kw.chung@koreacb.com>
2019-03-22 07:36:51 +0000

    modified

--- start-master.sh
+++ start-master.sh
@@ -9,8 +9,12 @@ pyspark &
 /livy/livy-0.5.0-incubating-bin/bin/livy-server start &
 
 /spark/bin/spark-class org.apache.spark.deploy.master.Master \
-#    --ip $SPARK_LOCAL_IP \
     --port $SPARK_MASTER_PORT \
     --webui-port $SPARK_MASTER_WEBUI_PORT \
+
+#/spark/bin/spark-class org.apache.spark.deploy.master.Master \
+#    --ip $SPARK_LOCAL_IP \
+#    --port $SPARK_MASTER_PORT \
+#    --webui-port $SPARK_MASTER_WEBUI_PORT \
 
 #jupyter notebook --ip 0.0.0.0 --port 8888 --allow-root
